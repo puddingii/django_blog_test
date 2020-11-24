7ced5e77f0b0363be9598f038acfd9ac56b1442c
puddingii <67890897+puddingii@users.noreply.github.com>
2020-11-25 03:10:09 +0900

    Add options + change
    add pagination, delete option
    change base.html
    1 page => 4 poster

--- geonyeongsite/blog/urls.py
+++ geonyeongsite/blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('<int:blog_id>', views.detail, name='detail'),
     path('enroll/',views.enroll, name='enroll'),
     path('create/',views.create, name='create'),
+    path('<int:blog_id>/delete', views.delete, name='delete'),
 ]
--- geonyeongsite/blog/views.py
+++ geonyeongsite/blog/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.core.paginator import Paginator
 from .models import Blog
 
 # Create your views here.
 def home(request):
     blogs = Blog.objects
-    return render(request, 'blog/home.html', {'blogs':blogs})
+    blog_list = Blog.objects.all()
+    paginator = Paginator(blog_list, 4)
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+    return render(request, 'blog/home.html', {'blogs':blogs, 'posts':posts})
 
 def enroll(request):
     return render(request, 'blog/enroll.html')
@@ -21,6 +26,11 @@ def create(request):
 def detail(request, blog_id):
     blog_detail = get_object_or_404(Blog, pk=blog_id)
     return render(request, 'blog/detail.html', {'blog':blog_detail})
+
+def delete(request, blog_id):
+    blog = Blog.objects.get(id=blog_id)
+    blog.delete()
+    return redirect('/')
 
 
 '''def result(request):
